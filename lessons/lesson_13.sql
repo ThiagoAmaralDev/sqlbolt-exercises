2adc5a3ad37c13f7d5798441ba050d019c132a28
Thiago Amaral <thiagoamaraldev@gmail.com>
2026-05-28 09:46:37 -0300

    feat: termina a outra metade da lição 13 (inserting rows)

--- lessons/lesson_13.sql
+++ lessons/lesson_13.sql
@@ -8,4 +8,8 @@
 INSERT INTO Movies
 VALUES (15, 'Toy Story 4', 'John Lasseter', 2010, 90);
 
--- 2- ...
+-- 2- Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. Add the record to the BoxOffice table. 
+-- (Toy Story 4 foi lançado com grande aclamação da crítica! Obteve uma classificação de 8,7 e arrecadou 340 milhões nos Estados Unidos e 270 milhões internacionalmente. Adicione o recorde à tabela da bilheteria.)
+
+INSERT INTO boxoffice (movie_id, rating, domestic_sales, international_sales)
+VALUES (15, 8.7, 340000000, 270000000);
