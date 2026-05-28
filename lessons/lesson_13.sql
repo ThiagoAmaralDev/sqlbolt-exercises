6a1fcf7ccdcc3edda22c2cf5e7db2269d93f7673
Thiago Amaral <thiagoamaraldev@gmail.com>
2026-05-27 22:48:48 -0300

    feat: adiciona a metade da resolucao da licao 13 (inserting rows)

---
+++ lessons/lesson_13.sql
@@ -0,0 +1,11 @@
+-- ==========================================
+-- Plataforma: SQLBolt (https://sqlbolt.com/lesson/inserting_rows)
+-- LIÇÃO 13: Inserting rows
+-- ==========================================
+
+-- 1- Add the studio's new production, Toy Story 4 to the list of movies (you can use any director) (Adicione a nova produção do estúdio, Toy Story 4, à lista de filmes (você pode usar qualquer diretor).)
+
+INSERT INTO Movies
+VALUES (15, 'Toy Story 4', 'John Lasseter', 2010, 90);
+
+-- 2- ...
